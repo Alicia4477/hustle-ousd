be44c97d7780fde749b74259510b31e7129a2fd7
Alicia Pablo <aliciapablo4477@Alicias-MacBook-Pro.local>
2025-08-20 17:48:57 -0700

    finished lab2

---
+++ lab2.py
@@ -0,0 +1,21 @@
+#lab 2: Alicia Pablo
+
+#step 1 : Name Variable
+name = "Alicia Pablo"
+
+#step 2 : Nickname Variable
+nickname = "Lisa"
+
+#step 3 : Age variable 
+# int = whole sum, float = decimal
+age = 18
+
+#step 4 : Boolen Variable 
+used_before = False
+
+#step 5 : List Data Structure
+hobbies = ["Drawing", "Volleyball", "hiking", "Travel"]
+
+#step 6: Dictionary (key:value) Pairs 
+my_favs = {"Drawing":"People","Travel":"San Diego","Hiking":["Emotions", "Mountains"]}
+        
